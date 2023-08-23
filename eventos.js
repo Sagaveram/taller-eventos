c987366f2c16f2e0c480bcbc3db09acf16b756e0
Sagaveram <110860320+Sagaveram@users.noreply.github.com>
2023-08-22 23:52:12 -0300

    cambios realizados

--- eventos.js
+++ eventos.js
@@ -1,5 +1,14 @@
 const divs=document.getElementById("div1");
+const boton=document.getElementById("boton");
 function saludoDiv(){
-    alert("Hola! Soy el div")
+    alert("Hola! Soy el div");
+}
+
+function saludoBoton(event){
+    event.stopPropagation();
+    alert("Hola!");
+
 }
 divs.addEventListener("click", saludoDiv);
+
+boton.addEventListener("click",saludoBoton);
